5c53e1de4d02dbb569b32178e31dbcfe11cd2bf6
Shrutiben <shethshruti17@gmail.com>
2025-05-19 11:56:22 +0530

    Publish the Project On local IIS

--- Program.cs
+++ Program.cs
@@ -27,7 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
